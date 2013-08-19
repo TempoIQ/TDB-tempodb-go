df0d65adc74c07354f50e7aaa4862972115140d1
Blake Smith <blakesmith0@gmail.com>
2013-08-19 13:38:33 -0500

    Add secure option to client, simplify url building

--- tempodb.go
+++ tempodb.go
@@ -17,7 +17,7 @@ var (
 )
 
 const (
-	API_HOSTNAME    = "https://api.tempo-db.com"
+	API_HOSTNAME    = "api.tempo-db.com"
 	API_SECURE_PORT = 443
 	ISO8601_FMT     = "2006-01-02T15:04:05.000Z0700"
 	VERSION         = 0.1
@@ -41,18 +41,20 @@ type Client struct {
 	Host    string
 	Port    int
 	Remoter Remoter
+	Secure  bool
 }
 
 //Create a new client instance. Must provide your TempoDB API key and secret.
 func NewClient(key string, secret string) *Client {
 	client := &Client{Key: key, Secret: secret, Host: API_HOSTNAME, Port: API_SECURE_PORT}
 	client.Remoter = &http.Client{}
+	client.Secure = true
 	return client
 }
 
 //Gets a list of series filtered by the provided Filter.
 func (client *Client) GetSeries(filter *Filter) ([]*Series, error) {
-	url := client.buildUrl("/series?", "", filter.Url().Encode())
+	url := client.buildUrl("/series?", filter.Url().Encode())
 	resp, err := client.makeRequest(url, "GET", []byte{})
 	if err != nil {
 		return nil, err
@@ -87,7 +89,7 @@ func (client *Client) CreateSeries(key string) (*Series, error) {
 	if err != nil {
 		return nil, err
 	}
-	url := client.buildUrl("/series/", "", "")
+	url := client.buildUrl("/series/", "")
 	resp, err := client.makeRequest(url, "POST", reqBody)
 	if err != nil {
 		return nil, err
@@ -114,7 +116,7 @@ func (client *Client) CreateSeries(key string) (*Series, error) {
 //Updates a Series's metadata.
 func (client *Client) UpdateSeries(series *Series) (*Series, error) {
 	endpointUrl := fmt.Sprintf("/series/id/%s/", url.QueryEscape(series.Id))
-	url := client.buildUrl(endpointUrl, "", "")
+	url := client.buildUrl(endpointUrl, "")
 	b, err := json.Marshal(series)
 	if err != nil {
 		return nil, err
@@ -152,7 +154,7 @@ func (client *Client) WriteKey(key string, data []*DataPoint) error {
 
 //Writes a set of datapoints for different series for the same timestamp.
 func (client *Client) WriteBulk(ts time.Time, data []BulkPoint) error {
-	url := client.buildUrl("/data/", "", "")
+	url := client.buildUrl("/data/", "")
 	dataSet := &BulkDataSet{
 		Ts:   ts,
 		Data: data,
@@ -179,7 +181,7 @@ func (client *Client) WriteBulk(ts time.Time, data []BulkPoint) error {
 
 //Reads a list of DataSet by the provided filter and rolluped by the interval
 func (client *Client) Read(start time.Time, end time.Time, filter *Filter, readOpts *ReadOptions) ([]*DataSet, error) {
-	url := client.buildUrl("/data?", client.encodeTimes(start, end), urlMerge(filter.Url(), readOpts.Url()).Encode())
+	url := client.buildUrl("/data?", urlMerge(client.encodeTimes(start, end), filter.Url(), readOpts.Url()).Encode())
 	resp, err := client.makeRequest(url, "GET", []byte{})
 	if err != nil {
 		return nil, err
@@ -223,7 +225,7 @@ func (client *Client) IncrementKey(key string, data []*DataPoint) error {
 
 //Increments a set of datapoints for different series for the same timestamp.
 func (client *Client) IncrementBulk(ts time.Time, data []BulkPoint) error {
-	url := client.buildUrl("/increment/", "", "")
+	url := client.buildUrl("/increment/", "")
 	dataSet := &BulkDataSet{
 		Ts:   ts,
 		Data: data,
@@ -260,7 +262,7 @@ func (client *Client) DeleteKey(key string, start time.Time, end time.Time) erro
 
 func (client *Client) readSeries(series_type string, seriesVal string, start time.Time, end time.Time, readOpts *ReadOptions) (*DataSet, error) {
 	endpointUrl := fmt.Sprintf("/series/%s/%s/data/?", series_type, url.QueryEscape(seriesVal))
-	url := client.buildUrl(endpointUrl, client.encodeTimes(start, end), readOpts.Url().Encode())
+	url := client.buildUrl(endpointUrl, urlMerge(client.encodeTimes(start, end), readOpts.Url()).Encode())
 	resp, err := client.makeRequest(url, "GET", []byte{})
 	if err != nil {
 		return nil, err
@@ -292,7 +294,7 @@ func (client *Client) writeSeries(series_type string, seriesVal string, data []*
 	if err != nil {
 		return err
 	}
-	url := client.buildUrl(endpointUrl, "", "")
+	url := client.buildUrl(endpointUrl, "")
 	resp, err := client.makeRequest(url, "POST", b)
 	if err != nil {
 		return err
@@ -316,7 +318,7 @@ func (client *Client) incrementSeries(series_type string, seriesVal string, data
 	if err != nil {
 		return err
 	}
-	url := client.buildUrl(endpointUrl, "", "")
+	url := client.buildUrl(endpointUrl, "")
 	resp, err := client.makeRequest(url, "POST", b)
 	if err != nil {
 		return err
@@ -335,7 +337,7 @@ func (client *Client) incrementSeries(series_type string, seriesVal string, data
 
 func (client *Client) deleteSeries(series_type string, seriesVal string, start time.Time, end time.Time) error {
 	endpointUrl := fmt.Sprintf("/series/%s/%s/data/?", series_type, url.QueryEscape(seriesVal))
-	url := client.buildUrl(endpointUrl, client.encodeTimes(start, end), "")
+	url := client.buildUrl(endpointUrl, client.encodeTimes(start, end).Encode())
 	resp, err := client.makeRequest(url, "DELETE", []byte{})
 	if err != nil {
 		return err
@@ -352,22 +354,25 @@ func (client *Client) deleteSeries(series_type string, seriesVal string, start t
 	return nil
 }
 
-func (client *Client) buildUrl(endpoint string, times string, paramsStr string) string {
-	if times == "" {
-		return client.Host + "/v1" + endpoint + paramsStr
+func (client *Client) buildUrl(endpoint string, paramsStr string) string {
+	var proto string
+	if client.Secure {
+		proto = "https://"
+	} else {
+		proto = "http://"
 	}
 
-	return client.Host + "/v1" + endpoint + times + "&" + paramsStr
+	return proto + client.Host + "/v1" + endpoint + paramsStr
 }
 
-func (client *Client) encodeTimes(start time.Time, end time.Time) string {
+func (client *Client) encodeTimes(start time.Time, end time.Time) url.Values {
 	v := url.Values{}
 	startStr := start.Format(ISO8601_FMT)
 	endStr := end.Format(ISO8601_FMT)
 	v.Add("start", startStr)
 	v.Add("end", endStr)
 
-	return v.Encode()
+	return v
 }
 
 func (client *Client) makeRequest(builtUrl string, method string, formString []byte) (*http.Response, error) {
